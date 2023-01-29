68d401ed6d0c2366b93753f484b5cd78b3287690
LeoLi225 <0319leo@gmail.com>
2023-01-29 15:03:51 -0800

    add Ming code to branch

---
+++ SE2.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace seleniumTest.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string url = "https://localhost:44351//Clients";
+       //        string url = "https://localhost:44351";
+            //string url = "http://localhost:49198";
+            ChromeDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+     //       driver.FindElement(By.LinkText("Client")).Click();
+     //       driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Create New")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.Id("Name")).SendKeys("Marge Simpson");
+            driver.FindElement(By.Id("Address")).SendKeys("Springfield");
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
+    }
+}
